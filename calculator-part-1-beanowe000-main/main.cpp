0a688ade89b9233038418d99419fc01306d67b0f
beanowe000 <157844642+beanowe000@users.noreply.github.com>
2024-09-10 08:22:21 -0600

    part 1

---
+++ calculator-part-1-beanowe000-main/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+
+    int first = 0;
+    int second = 0;
+
+    cout << "Input First Number:";
+    cin >> first;
+    cout << "Input Second Number:";
+    cin >> second;
+    
+     int both = first+second;
+    cout << both;
+}
